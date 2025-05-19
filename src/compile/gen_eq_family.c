61e25a2d290666f69d3de027b9400a3e5f1f274a
Vidith Madhu <vidhmadh1892@gmail.com>
2025-05-19 00:35:46 -0500

    small fix

--- src/compile/gen_eq_family.c
+++ src/compile/gen_eq_family.c
@@ -91,6 +91,7 @@ int gen_eq_family (block blk, symstack stk, char *in_buf, size_t *frame_size,
                     out_file, msg);
                 if (ret) { goto done; }
             } else {
+                TYPE_CHECK (lsym_info.category, rsym_info.category);
                 if (rsym_info.category == INT64) {
                     ret = gen_eq_int (blk, rsym_info, lsym_info, in_buf,
                         out_file, msg);
